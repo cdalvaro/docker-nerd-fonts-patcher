edc2bac4c64435624ee5f2ae67e0cc992cf60983
Carlos Álvaro <carlos@cdalvaro.io>
2019-09-08 19:31:44 +0200

    feat: Fix libspiro version to release 20190731

--- assets/build/install.sh
+++ assets/build/install.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Install libspiro
-git clone --depth 1 https://github.com/fontforge/libspiro.git && \
+git clone --branch "${LIBSPIRO_VERSION}" --depth 1 https://github.com/fontforge/libspiro.git && \
     cd libspiro && \
     autoreconf -i && \
     automake --foreign -Wall && \
