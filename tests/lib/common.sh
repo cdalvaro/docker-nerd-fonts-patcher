8a4c6eae83e124c3c3570d06b3d8cf222fcecf0a
Carlos Álvaro <carlos@cdalvaro.io>
2022-08-28 11:45:24 +0200

    feat(ci): Add tests for pull requests

---
+++ tests/lib/common.sh
@@ -0,0 +1,160 @@
+#!/usr/bin/env bash
+
+#---  ENV VARIABLE  ---------------------------------------------------------------------------------------------------
+#          NAME:  IMAGE_NAME
+#   DESCRIPTION:  The name and tag of the Docker image. Default: 'cdalvaro/docker-nerd-fonts-patcher:latest'.
+#----------------------------------------------------------------------------------------------------------------------
+export IMAGE_NAME=${IMAGE_NAME:-'cdalvaro/docker-nerd-fonts-patcher:latest'}
+
+#---  ENV VARIABLE  ---------------------------------------------------------------------------------------------------
+#          NAME:  PLATFORM
+#   DESCRIPTION:  The platform to run the tests on. Default: the current platform.
+#----------------------------------------------------------------------------------------------------------------------
+export PLATFORM=${PLATFORM:-$(docker version --format='{{.Server.Os}}/{{.Server.Arch}}')}
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  lowercase
+#   DESCRIPTION:  Lowercase a string.
+#----------------------------------------------------------------------------------------------------------------------
+function lowercase() {
+    echo "$1" | tr '[:upper:]' '[:lower:]'
+}
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  log_debug
+#   DESCRIPTION:  Echo debug information to stdout.
+#----------------------------------------------------------------------------------------------------------------------
+function log_debug() {
+  if [[ $(lowercase "${DEBUG}") == true || $(lowercase "${ECHO_DEBUG}") == true ]]; then
+    echo "[DEBUG] - $*"
+  fi
+}
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  log_info
+#   DESCRIPTION:  Echo information to stdout.
+#----------------------------------------------------------------------------------------------------------------------
+function log_info() {
+  echo "[INFO] - $*"
+}
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  log_warn
+#   DESCRIPTION:  Echo warning information to stdout.
+#----------------------------------------------------------------------------------------------------------------------
+function log_warn() {
+  (echo >&2 "[WARN] - $*")
+}
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  log_error
+#   DESCRIPTION:  Echo errors to stderr.
+#----------------------------------------------------------------------------------------------------------------------
+function log_error() {
+  (echo >&2 "[ERROR] - $*")
+}
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  is_arm32
+#   DESCRIPTION:  Check whether the platform is ARM 32-bits or not.
+#----------------------------------------------------------------------------------------------------------------------
+function is_arm32() {
+  uname -m | grep -qE 'armv7l'
+}
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  is_arm32
+#   DESCRIPTION:  Check whether the platform is ARM 64-bits or not.
+#----------------------------------------------------------------------------------------------------------------------
+function is_arm64() {
+  uname -m | grep -qE 'arm64|aarch64'
+}
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  is_arm32
+#   DESCRIPTION:  Check whether the platform is ARM or not.
+#----------------------------------------------------------------------------------------------------------------------
+function is_arm() {
+  is_arm32 || is_arm64
+}
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  ok
+#   DESCRIPTION:  Print a successfull message.
+#----------------------------------------------------------------------------------------------------------------------
+function ok()
+{
+  echo "✅ $*"
+}
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  error
+#   DESCRIPTION:  Print an error message, show the salt-master log and exit with code 1.
+#----------------------------------------------------------------------------------------------------------------------
+function error()
+{
+  echo "🔥 $*"
+  return 1
+}
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  download
+#   DESCRIPTION:  Download the content from the given URL and save it into the specified file.
+#----------------------------------------------------------------------------------------------------------------------
+function download() {
+  local URL="$1"; shift
+  local FILE_NAME="$1"; shift
+
+  local WGET_ARGS=(--quiet)
+  is_arm32 && WGET_ARGS+=(--no-check-certificate)
+
+  log_info "Downloading ${FILE_NAME} from ${URL} ..."
+  wget "${WGET_ARGS[@]}" "$@" -O "${FILE_NAME}" "${URL}"
+  if [[ -f "${FILE_NAME}" ]]; then
+    log_debug "Success!"
+  else
+    log_error "Failed to download ${URL}"
+    return 1
+  fi
+}
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  check_sha256
+#   DESCRIPTION:  Compute the SHA256 hash for the given file and check if it matches the expected one.
+#----------------------------------------------------------------------------------------------------------------------
+function check_sha256() {
+  local FILE="${1}"
+  local SHA256="${2}"
+
+  log_info "Checking ${FILE} SHA256 hash ..."
+  if echo "${SHA256}  ${FILE}" | shasum -a 256 -c --status -; then
+    log_debug "SHA256 hash for ${FILE} matches! (${SHA256})"
+  else
+    local HASH
+    HASH=$(shasum -a 256 "${FILE}" | awk '{print $1}')
+    log_error "SHA256 checksum mismatch for ${FILE}"
+    log_error "Expected: ${SHA256}"
+    log_error "     Got: ${HASH}"
+    return 1
+  fi
+}
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  check_equal
+#   DESCRIPTION:  Check if the given value is equal to the expected value.
+#----------------------------------------------------------------------------------------------------------------------
+function check_equal()
+{
+  local current="$1"
+  local expected="$2"
+  local message="$3"
+
+  output=$(cat <<EOF
+${message}
+  Expected: ${expected}
+   Current: ${current}
+EOF
+)
+
+  [[ "${current}" == "${expected}" ]] && ok "${output}" || error "${output}"
+}
---
+++ tests/patch-test.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -e
+
+# https://stackoverflow.com/a/4774063/3398062
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+COMMON_FILE="${SCRIPT_PATH}/lib/common.sh"
+
+# shellcheck source=tests/lib/common.sh
+source "${COMMON_FILE}"
+
+NERDFONTS_VERSION="$(cat VERSION)"
+
+WORK_DIR="${SCRIPT_PATH}/assets"
+rm -rf "${WORK_DIR}" && mkdir -p "${WORK_DIR}"
+pushd "${WORK_DIR}"
+
+INPUT_DIR="$(pwd)/in"
+OUTPUT_DIR="$(pwd)/out"
+
+[ "$(lowercase "${DEBUG}")" == true ] && set -vx
+log_info "🧪 Running patch tests ..."
+
+FIRACODE_VERSION="6.2"
+FIRACODE_SHA256="0949915ba8eb24d89fd93d10a7ff623f42830d7c5ffc3ecbf960e4ecad3e3e79"
+FIRACODE_FILE_NAME="FiraCode.zip"
+FIRACODE_URL="https://github.com/tonsky/FiraCode/releases/download/${FIRACODE_VERSION}/Fira_Code_v${FIRACODE_VERSION}.zip"
+
+log_info "==> Downloading original Fira Code ${FIRACODE_VERSION} font ..."
+download "${FIRACODE_URL}" "${FIRACODE_FILE_NAME}"
+check_sha256 "${FIRACODE_FILE_NAME}" "${FIRACODE_SHA256}" || error "SHA256 checksum mismatch for ${FIRACODE_URL}"
+
+mkdir -p "${INPUT_DIR}/" "${OUTPUT_DIR}/"
+unzip -q "${FIRACODE_FILE_NAME}" 'ttf/*' -d "${INPUT_DIR}/"
+mv "${INPUT_DIR}"/ttf/* "${INPUT_DIR}/"
+rmdir "${INPUT_DIR}/ttf"
+
+log_info "==> Patching original Fira Code font ..."
+docker run --rm \
+		--volume "${INPUT_DIR}/":/nerd-fonts/in \
+    --volume "${OUTPUT_DIR}/":/nerd-fonts/out \
+		--user "$(id -u)":"$(id -g)" \
+		-- \
+    cdalvaro/docker-nerd-fonts-patcher:latest \
+      --quiet --no-progressbars --mono --complete
+
+log_info "==> Downloading patched Fira Code font ..."
+FIRACODE_NERD_FONT_SHA256="7ad2fdab3e95405b45644425d74238eb8463f2547ad6ff076bcc383ccba1c9c6"
+FIRACODE_NERD_FONT_FILE_NAME="FiraCodeNerdFont.zip"
+FIRACODE_NERD_FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v${NERDFONTS_VERSION}/FiraCode.zip"
+download "${FIRACODE_NERD_FONT_URL}" "${FIRACODE_NERD_FONT_FILE_NAME}"
+check_sha256 "${FIRACODE_NERD_FONT_FILE_NAME}" "${FIRACODE_NERD_FONT_SHA256}" || error "SHA256 checksum mismatch for ${FIRACODE_NERD_FONT_URL}"
+
+log_info "==> Comparing own patched fonts with nerd font patched fonts ..."
+mkdir -p patched/
+unzip -q "${FIRACODE_NERD_FONT_FILE_NAME}" -d patched/
+
+while IFS='' read -r FONT; do
+  output_ttx="$(ttx -o "${INPUT_DIR}/${FONT}")"
+  patched_ttx="$(ttx -o "$(pwd)/patched/${FONT}")"
+  check_equal "${output_ttx}" "${patched_ttx}" "ttx output matches for font ${FONT}"
+done < <(find "${OUTPUT_DIR}" -type f -iname '*.ttf' -exec basename {} \; 2>/dev/null)
+
+popd
