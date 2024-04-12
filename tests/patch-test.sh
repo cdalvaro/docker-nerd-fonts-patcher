5e606d85c30ea79b47f24227c867f5b010c77406
Carlos Álvaro <carlos@cdalvaro.io>
2024-04-12 18:47:54 +0200

    upgrade: nerd-fonts 3.2.1

--- tests/patch-test.sh
+++ tests/patch-test.sh
@@ -28,18 +28,15 @@ check_regex "${FONTPATCHER_VERSION}" "${NERDFONTS_VERSION}"
 ### GIVEN
 TEST_FONT_NAME="FiraCode"
 
-TEST_FONT_SHA256="0949915ba8eb24d89fd93d10a7ff623f42830d7c5ffc3ecbf960e4ecad3e3e79"
 TEST_FONT_FILE_NAME="${TEST_FONT_NAME}Original.zip"
 TEST_FONT_URL="https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip"
 
-NERDFONT_FONT_SHA256="e70d4a8be94ed056ae0deed2c24c3389f87ccc1ecb08b4c3db9d39ce08840a54"
 NERDFONT_FONT_FILE_NAME="${TEST_FONT_NAME}NerdFont.zip"
 NERDFONT_FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v${NERDFONTS_VERSION}/${TEST_FONT_NAME}.zip"
 
 ### THEN
 log_info "==> Downloading original ${TEST_FONT_NAME} (${TEST_FONT_URL}) font ..."
 download "${TEST_FONT_URL}" "${TEST_FONT_FILE_NAME}"
-check_sha256 "${TEST_FONT_FILE_NAME}" "${TEST_FONT_SHA256}" || error "SHA256 checksum mismatch for ${TEST_FONT_URL}"
 
 TEST_FONT_DIRECTORY="$(mktemp -d)"
 unzip -q "${TEST_FONT_FILE_NAME}" -d "${TEST_FONT_DIRECTORY}/"
@@ -50,7 +47,6 @@ font_patcher --quiet --no-progressbars --complete 2>/dev/null
 
 log_info "==> Downloading patched ${TEST_FONT_NAME} font ..."
 download "${NERDFONT_FONT_URL}" "${NERDFONT_FONT_FILE_NAME}"
-check_sha256 "${NERDFONT_FONT_FILE_NAME}" "${NERDFONT_FONT_SHA256}" || error "SHA256 checksum mismatch for ${NERDFONT_FONT_URL}"
 
 log_info "==> Comparing own patched fonts with nerd font patched fonts ..."
 mkdir -p patched/
